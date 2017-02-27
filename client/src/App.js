2d2a41aa9a92f96bc80f139350c44853009fb55c
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-02-27 09:12:54 -0700

    Added initial SearchBar component
    Added SearchBar component will be used to filter through results

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import SearchBar from './components/search/SearchBar';
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +15,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div>
+          <SearchBar />
+        </div>
       </div>
     );
   }
