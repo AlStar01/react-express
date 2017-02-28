0e9e97dcc67e2422c76b4116dfde4a38dae1750b
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-02-28 09:14:22 -0700

    Added basic material structure including sidenav and toolbar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 
 import CARS from './components/table/cars';
 
@@ -9,18 +10,24 @@ import FilterableCarTable from './components/table/FilterableCarTable/Filterable
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <div className="App-content">
-          <FilterableCarTable cars={CARS} />
-        </div>
+      <div>
+        <Layout fixedHeader>
+          <Header title={<span>React</span>}>
+            <Navigation>
+              <a href="">Link</a>
+            </Navigation>
+          </Header>
+          <Drawer title="Navigation">
+              <Navigation>
+                <a href="">Link</a>
+              </Navigation>
+          </Drawer>
+          <Content>
+            <div className="App-content">
+              <FilterableCarTable cars={CARS} />
+            </div>
+          </Content>
+        </Layout>
       </div>
     );
   }
