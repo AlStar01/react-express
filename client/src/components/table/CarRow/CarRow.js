1fb9bf6c52219be7cf73b54b205e63f1aca4f38e
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-03-23 09:42:04 -0600

    Updated car row to display Bootstrap tooltip for additional fuel economy information.

--- client/src/components/table/CarRow/CarRow.js
+++ client/src/components/table/CarRow/CarRow.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import './CarRow.css';
+
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 class CarRow extends Component {
     render() {
         var car = this.props.car;
+
+        const tooltip = <Tooltip id="tooltip">City: {car.city_fuel_economy}, Highway: {car.highway_fuel_economy}</Tooltip>;
         
         return (
-            <tr>
+            <tr className="carRow">
                 <td>{car.make}</td>
                 <td>{car.model}</td>
                 <td>{car.displacement}</td>
                 <td>{car.cylinders}</td>
-                <td>{car.combined_fuel_economy}</td>
+                <td>
+                    <OverlayTrigger overlay={tooltip}>
+                        <u className="text-info">{car.combined_fuel_economy}</u>
+                    </OverlayTrigger>
+                </td>
             </tr>
         );
     }
